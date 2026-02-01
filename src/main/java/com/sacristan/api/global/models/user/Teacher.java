21ecd844ddf01df7348867cf05d766417ed35e5f
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-02-01 22:14:38 +0100

    F54 - Relation ready

--- src/main/java/com/sacristan/api/global/models/user/Teacher.java
+++ src/main/java/com/sacristan/api/global/models/user/Teacher.java
@@ -3,6 +3,9 @@ package com.sacristan.api.global.models.user;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter
@@ -17,5 +20,21 @@ public class Teacher {
     @JoinColumn(name = "user_id", unique = true, nullable = false)
     @MapsId // Establishes that the Entity's ID and the referenced Entity's ID must be the same
     private User user;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "teacher")
+    @Builder.Default
+    private List<Student> students = new ArrayList<>();
+
+    /* -- HELPERS -- */
+    public void addStudent(Student student) {
+        students.add(student);
+        student.setTeacher(this);
+    }
+
+    public void removeStudent(Student student) {
+        students.remove(student);
+        student.setTeacher(null);
+    }
+
 
 }
