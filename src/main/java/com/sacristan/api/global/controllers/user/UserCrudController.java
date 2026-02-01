535e0f3fac21f3d054b5d4ce25ed6265cf19eeca
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-02-01 23:55:43 +0100

    Create user documented

--- src/main/java/com/sacristan/api/global/controllers/user/UserCrudController.java
+++ src/main/java/com/sacristan/api/global/controllers/user/UserCrudController.java
@@ -5,6 +5,12 @@ import com.sacristan.api.global.dtos.user.UpdateUser;
 import com.sacristan.api.global.dtos.user.UserResponse;
 import com.sacristan.api.global.services.user.UserCrudService;
 import com.sacristan.api.global.services.user.UserUtilsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
+@Tag(name = "User CRUD controller", description = "CRUD Controller for basic User entity")
 public class UserCrudController {
 
     private final UserCrudService service;
@@ -22,8 +29,73 @@ public class UserCrudController {
 
 
     @PostMapping
+    @ApiResponse(
+            responseCode = "201",
+            description = "User created successfully",
+            content = @Content(
+                    mediaType = "application/JSON",
+                    schema = @Schema(implementation = UserResponse.class),
+                    examples = {
+                            @ExampleObject(
+                                    value = """
+                                            {
+                                                "id": 1,
+                                                "name": "John",
+                                                "lastName": "Doe",
+                                            }
+                                            """
+                            )
+                    }
+            )
+    )
+    @ApiResponse(
+            responseCode = "400",
+            description = "Illegal Arguments on request",
+            content = @Content(
+                    mediaType = "application/JSON",
+                    examples = {
+                            @ExampleObject(
+                                    value = """
+                                            {
+                                              "type": "about:blank",
+                                              "title": "Passwords don't match",
+                                              "status": 400,
+                                              "detail": "Passwords provided in 'password' and 'verifyPassword' fields do not match.",
+                                              "instance": "/api/v1/user"
+                                            }
+                                            """
+                            )
+                    }
+            )
+    )
+    @Operation(
+            summary = "Create a new User",
+            description = "Creates a new User in the system with the provided information."
+    )
     public ResponseEntity<UserResponse> create(
-            @RequestBody CreateUser createUser
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "CreateUser DTO object containing the information needed to create a new User",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/JSON",
+                            schema = @Schema(implementation = CreateUser.class),
+                            examples = {
+                                    @ExampleObject(
+                                            value = """
+                                                    {
+                                                        "name": "John",
+                                                        "lastName": "Doe",
+                                                        "email": "doe_john@testmail.com",
+                                                        "username": "johndoe",
+                                                        "password": "password123",
+                                                        "verifyPassword": "password123"
+                                                    }
+                                                    """
+                                    )
+                            }
+                    )
+            )
+            @RequestBody(required = true) CreateUser createUser
     ) {
 
         if (!utilsService.passwordMatch(createUser.password(), createUser.verifyPassword()))
