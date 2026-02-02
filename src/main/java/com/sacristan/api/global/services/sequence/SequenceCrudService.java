d5b9735a04ffb0a282ebab9dc6f31a50d13c3b17
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-02 17:22:46 +0100

    Crud terminad

---
+++ src/main/java/com/sacristan/api/global/services/sequence/SequenceCrudService.java
@@ -0,0 +1,50 @@
+package com.sacristan.api.global.services.sequence;
+
+import com.sacristan.api.global.models.Category;
+import com.sacristan.api.global.models.Sequence;
+import com.sacristan.api.global.repositories.CategoryRepository;
+import com.sacristan.api.global.repositories.SequenceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class SequenceCrudService {
+
+    private final SequenceRepository repository;
+    private final CategoryRepository categoryRepository;
+
+
+    public List<Sequence> getAllSequences() {
+        return repository.findAll();
+    }
+
+    public Sequence read(Long id) {
+        return repository.findById(id).orElseThrow(() -> new NoSuchElementException("Sequence not found with id: " + id));
+    }
+
+    public Sequence createSequence(Sequence sequence) {
+
+        if (sequence.getCategory() != null && sequence.getCategory().getId() != null) {
+            Category category = categoryRepository.findById(sequence.getCategory().getId())
+                    .orElseThrow(() -> new NoSuchElementException("Sequence not found with id: " + sequence.getCategory().getId()));
+            sequence.setCategory(category);
+        }
+
+        return repository.save(sequence);
+    }
+
+    public Sequence updateSequence(Long id, Sequence newSequence){
+        Sequence sequence = repository.findById(id).orElseThrow(() -> new NoSuchElementException("Sequence not found with id: " + id));
+
+        return repository.save(sequence.modify(newSequence));
+    }
+
+    public void deleteSequence(Long id) {
+        Sequence sequence = repository.findById(id).orElseThrow(() -> new NoSuchElementException("Sequence not found with id: " + id));
+        repository.delete(sequence);
+    }
+}
