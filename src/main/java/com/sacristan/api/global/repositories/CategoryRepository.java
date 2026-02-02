241922d2e8a89d675026620dcb04af37f9a4c4f5
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-02 16:43:33 +0100

    Crud para controlar las categorias con consultas para lanzar excepciones en caso de que fuera necesario

---
+++ src/main/java/com/sacristan/api/global/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.sacristan.api.global.repositories;
+
+import com.sacristan.api.global.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>, JpaSpecificationExecutor<Category> {
+}
---
+++ src/main/java/com/sacristan/api/global/services/category/CategoryCrudService.java
@@ -0,0 +1,62 @@
+package com.sacristan.api.global.services.category;
+
+import com.sacristan.api.error.BadRequestException;
+import com.sacristan.api.global.models.Category;
+import com.sacristan.api.global.models.Sequence;
+import com.sacristan.api.global.models.user.User;
+import com.sacristan.api.global.models.user.extra.Role;
+import com.sacristan.api.global.repositories.CategoryRepository;
+import com.sacristan.api.global.repositories.RoutineRepository;
+import com.sacristan.api.global.repositories.SequenceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryCrudService {
+
+    private final CategoryRepository repository;
+    private final SequenceRepository sequenceRepository;
+    private final RoutineRepository routineRepository;
+
+    public Category create(Category category) {
+
+        return repository.save(category);
+    }
+
+    public Category read(long id) {
+        return repository
+                .findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Category not found with id: " + id));
+    }
+
+    public void delete(long id) {
+        Category category = repository.findById(id).orElseThrow(()-> new NoSuchElementException("Category not found with id: " + id));
+
+        if (sequenceRepository.existsByCategory(category) || routineRepository.existsByCategory(category))
+            throw new BadRequestException("Cannot delete Category with assigned sequences or routines");
+
+        repository.delete(category);
+    }
+
+    public Page<Category> list(Pageable pageable) {
+
+        Page<Category> categories = repository.findAll(pageable);
+
+        if (categories.isEmpty())
+            throw new NoSuchElementException("No Categories found");
+
+        return categories;
+    }
+
+    public Category update(Long id, Category newCategory){
+        Category category = repository.findById(id).orElseThrow(() -> new NoSuchElementException("Category not found with id: " + id));
+
+        return repository.save(category.modify(newCategory));
+    }
+
+}
