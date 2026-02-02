dd4e6fa9c674db2d0a7b3b778ec13f49d7ed526c
mauroz9 <mauro.serranoo26@gmail.com>
2026-02-02 09:39:19 +0100

    Entidad base creada

---
+++ src/main/java/com/sacristan/api/global/models/DaysOfTheWeek.java
@@ -0,0 +1,5 @@
+package com.sacristan.api.global.models;
+
+public enum DaysOfTheWeek {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
---
+++ src/main/java/com/sacristan/api/global/models/Rutine.java
@@ -0,0 +1,27 @@
+package com.sacristan.api.global.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "rutines")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Rutine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Set<DaysOfTheWeek> days = new HashSet<>();
+}
