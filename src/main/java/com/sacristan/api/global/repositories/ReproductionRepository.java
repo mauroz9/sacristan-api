8594b04b7bf8c3122506976c44b48c1e93eece1a
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-03-04 19:55:37 +0100

    feat: filtrar rutinas

--- src/main/java/com/sacristan/api/global/repositories/ReproductionRepository.java
+++ src/main/java/com/sacristan/api/global/repositories/ReproductionRepository.java
@@ -2,6 +2,7 @@ package com.sacristan.api.global.repositories;
 
 import com.sacristan.api.global.models.Reproduction;
 import com.sacristan.api.global.models.Sequence;
+import com.sacristan.api.global.models.Status;
 import com.sacristan.api.global.models.user.Student;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,8 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -20,4 +23,16 @@ public interface ReproductionRepository extends JpaRepository<Reproduction, Long
     @Query("DELETE FROM Reproduction r WHERE r.routineSequence.id = :routineSequenceId")
     void deleteByRoutineSequenceId(Long routineSequenceId);
 
+
+    @Query(
+            "SELECT r FROM Reproduction r " +
+            "WHERE r.student.id = :userId " +
+            "AND r.routineSequence.id = :routineSequenceId " +
+            "AND r.startedAt >= :today AND r.startedAt < :tomorrow " +
+            "AND r.status = :completed"
+    )
+    List<Reproduction> findCompletedRoutineSequenceForToday(Long userId, Long routineSequenceId,
+                                                            LocalDateTime today,
+                                                            LocalDateTime tomorrow,
+                                                            Status completed);
 }
--- src/main/java/com/sacristan/api/interfaces/student/services/routine/StudentRoutineService.java
+++ src/main/java/com/sacristan/api/interfaces/student/services/routine/StudentRoutineService.java
@@ -1,10 +1,9 @@
 package com.sacristan.api.interfaces.student.services.routine;
 
-import com.sacristan.api.global.models.DaysOfTheWeek;
-import com.sacristan.api.global.models.Routine;
-import com.sacristan.api.global.models.RoutineSequence;
-import com.sacristan.api.global.models.Sequence;
+import com.sacristan.api.global.models.*;
 import com.sacristan.api.global.models.user.User;
+import com.sacristan.api.global.repositories.ReproductionRepository;
+import com.sacristan.api.global.repositories.RoutineSequenceRepository;
 import com.sacristan.api.interfaces.student.dtos.sequence.PrincipalSequenceDTO;
 import com.sacristan.api.interfaces.student.services.user.StudentUserUtilsService;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +23,7 @@ import java.util.List;
 public class StudentRoutineService {
 
     private final StudentUserUtilsService studentUserUtilsService;
+    private final ReproductionRepository reproductionRepository;
 
     public Page<PrincipalSequenceDTO> getPendingSequences(Pageable pageable, User user) {
         List<Routine> routines = studentUserUtilsService.getRoutinesByUserId(user);
@@ -31,6 +32,21 @@ public class StudentRoutineService {
                 .filter(r -> r.getDaysOfTheWeek().contains(DaysOfTheWeek.valueOf(LocalDate.now().getDayOfWeek().name())))
                 .map(Routine::getSequences)
                 .flatMap(List::stream)
+                .filter(rs -> {
+                    List<Reproduction> reproductions = reproductionRepository.findCompletedRoutineSequenceForToday(
+                            user.getId(),
+                            rs.getId(),
+                            LocalDate.now().atStartOfDay(),
+                            LocalDate.now().atStartOfDay().plusDays(1),
+                            Status.COMPLETED
+                    );
+                    return !reproductions
+                            .stream()
+                            .map(Reproduction::getRoutineSequence)
+                            .map(RoutineSequence::getId)
+                            .toList()
+                            .contains(rs.getId());
+                })
                 .filter(rs ->
                         rs.getStartTime().isAfter(LocalTime.now().minusMinutes(15))
                     && (rs.getStartTime().isBefore(LocalTime.now().plusMinutes(30)) && rs.getEndTime().isAfter(LocalTime.now()))
