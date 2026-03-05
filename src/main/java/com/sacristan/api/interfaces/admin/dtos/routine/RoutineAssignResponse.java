c12b2ffcc8942a1933b6a1491ccecb06070b0b9c
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-03-06 00:19:44 +0100

    feat: assign routine

---
+++ src/main/java/com/sacristan/api/interfaces/admin/dtos/routine/RoutineAssignResponse.java
@@ -0,0 +1,32 @@
+package com.sacristan.api.interfaces.admin.dtos.routine;
+
+import com.sacristan.api.global.models.DaysOfTheWeek;
+import com.sacristan.api.global.models.Routine;
+import com.sacristan.api.interfaces.admin.dtos.category.CategoryResponse;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record RoutineAssignResponse(
+        Long id,
+        String name,
+        Set<String> daysOfWeek,
+        CategoryResponse category
+) {
+    public static RoutineAssignResponse of(Routine routine) {
+        return new RoutineAssignResponse(
+                routine.getId(),
+                routine.getName(),
+                convertToLow(routine.getDaysOfTheWeek()),
+                routine.getCategory() != null ? CategoryResponse.of(routine.getCategory()) : null
+        );
+    }
+
+    private static Set<String> convertToLow(Set<DaysOfTheWeek> daysOfTheWeek) {
+        return daysOfTheWeek.stream()
+                .map(
+                day -> day.name().substring(0, 1).toLowerCase()
+                )
+                .collect(Collectors.toSet());
+    }
+}
