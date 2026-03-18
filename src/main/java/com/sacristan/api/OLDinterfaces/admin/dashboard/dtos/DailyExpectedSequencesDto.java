10fded3ecdc84944a02e9d3548117559cb9accfb
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-03-18 19:26:43 +0100

    .gitignore

--- src/main/java/com/sacristan/api/OLDinterfaces/admin/dashboard/dtos/DailyExpectedSequencesDto.java
+++
@@ -1,6 +0,0 @@
-package com.sacristan.api.OLDinterfaces.admin.dashboard.dtos;
-
-import java.time.LocalDate;
-
-public record DailyExpectedSequencesDto(LocalDate date, long expected, long completed) {
-}
--- src/main/java/com/sacristan/api/OLDinterfaces/admin/dashboard/dtos/LatestReproductionsDto.java
+++
@@ -1,7 +0,0 @@
-package com.sacristan.api.OLDinterfaces.admin.dashboard.dtos;
-
-import java.time.LocalDateTime;
-
-public record LatestReproductionsDto(String studentName, String sequenceTitle, LocalDateTime endedAt) {
-}
-
